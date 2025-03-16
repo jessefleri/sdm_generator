268493e0aa008bcf723037c2bcabeddeb465563c
jessefleri <32076973+jessefleri@users.noreply.github.com>
2025-03-15 20:45:11 -0600

    Apply style/linting
    Add tidyverse styling to application code

--- app.R
+++ app.R
@@ -1,51 +1,42 @@
 # Species Distribution Model Builder
 # A Shiny app that creates random forest-based SDMs from presence-only data
 
-# remove ID from model_Data frame
-
-
-# library(dismo)
 library(shiny)
 library(bslib)
 library(dplyr)
 library(readr)
-library(randomForest)
 library(ranger)
 library(leaflet)
-# library(raster)
 library(sf)
 library(geodata)
-
 
 ui <- page_sidebar(
   title = "Species Distribution Model Builder",
   theme = bs_theme(bootswatch = "flatly", primary = "#3e8d63"),
-  
   sidebar = sidebar(
     width = 300,
     fileInput("file", "Upload presence data (CSV)", accept = c(".csv")),
     selectInput("species_col", "Species column", choices = NULL),
     selectInput("lat_col", "Latitude column", choices = NULL),
     selectInput("lon_col", "Longitude column", choices = NULL),
-    
     checkboxGroupInput(
       "bioclim_vars",
       "Bioclim Variables:",
-      choices = c("Annual Mean Temperature" = "bio_1",
-                  "Annual Precipitation" = "bio_12",
-                  "Max Temperature of Warmest Month" = "bio_5",
-                  "Min Temperature of Coldest Month" = "bio_6"),
-      selected = c("bio_1", "bio_12")
+      choices = c(
+        "Annual Mean Temperature" = "bio_1",
+        "Annual Precipitation" = "bio_12",
+        "Max Temperature of Warmest Month" = "bio_5",
+        "Min Temperature of Coldest Month" = "bio_6"
       ),
+      selected = c("bio_1", "bio_12")
+    ),
     numericInput("ntree", "Number of trees:", 500, min = 100, max = 2000),
     actionButton("run_model", "Run Model", class = "btn-success btn-lg w-100")
   ),
-  
   layout_columns(
     card(card_header("Presence Data"), leafletOutput("map_presence", height = "350px")),
     card(card_header("Distribution Model"), leafletOutput("map_prediction", height = "350px"))
   ),
-  
   layout_columns(
     card(card_header("Variable Importance"), plotOutput("variable_importance", height = "250px")),
     card(card_header("Model Info"), verbatimTextOutput("model_summary"))
@@ -53,37 +44,39 @@ ui <- page_sidebar(
 )
 
 server <- function(input, output, session) {
-  
   # Reactive values
   presence_data <- reactiveVal(NULL)
   sdm_model <- reactiveVal(NULL)
   prediction_raster <- reactiveVal(NULL)
-  
+
   # Update selectors when file is uploaded
   observeEvent(input$file, {
     req(input$file)
     data <- read.csv(input$file$datapath)
     presence_data(data)
-    
+
     updateSelectInput(session, "species_col", choices = names(data))
     updateSelectInput(session, "lat_col", choices = names(data))
     updateSelectInput(session, "lon_col", choices = names(data))
-    
+
     # Auto-select columns based on common naming patterns
     for (col in names(data)) {
-      if (grepl("species|taxa|name", tolower(col)))
+      if (grepl("species|taxa|name", tolower(col))) {
         updateSelectInput(session, "species_col", selected = col)
-      if (grepl("lat|latitude", tolower(col)))
+      }
+      if (grepl("lat|latitude", tolower(col))) {
         updateSelectInput(session, "lat_col", selected = col)
-      if (grepl("lon|lng|longitude", tolower(col)))
+      }
+      if (grepl("lon|lng|longitude", tolower(col))) {
         updateSelectInput(session, "lon_col", selected = col)
+      }
     }
   })
-  
+
   # Display presence points map
   output$map_presence <- renderLeaflet({
     req(presence_data(), input$species_col, input$lat_col, input$lon_col)
-    
+
     data <- presence_data()
 
     leaflet() %>%
@@ -96,21 +89,23 @@ server <- function(input, output, session) {
         color = "#ff7f00",
         fillOpacity = 0.8,
         stroke = FALSE,
-        popup = ~paste("<b>Species:</b>", species,
-                       "<br><b>Lat:</b>", lat,
-                       "<br><b>Lon:</b>", lon)
+        popup = ~ paste(
+          "<b>Species:</b>", species,
+          "<br><b>Lat:</b>", lat,
+          "<br><b>Lon:</b>", lon
+        )
       )
   })
-  
+
   # Run model when button is clicked
   observeEvent(input$run_model, {
     req(presence_data(), input$species_col, input$lat_col, input$lon_col, input$bioclim_vars)
-    
-    withProgress(message = 'Building model...', value = 0, {
+
+    withProgress(message = "Building model...", value = 0, {
       # Process presence data
       incProgress(0.1, detail = "Processing presence data")
       data <- presence_data()
-      
+
       # Create spatial points
       presence_pts <- data.frame(
         species = data[[input$species_col]],
@@ -118,133 +113,98 @@ server <- function(input, output, session) {
         lat = data[[input$lat_col]]
       ) %>%
         st_as_sf(coords = c("lon", "lat"), crs = 4326)
-      
+
       coords <- data %>%
         select(lon = !!input$lon_col, lat = !!input$lat_col) %>%
         na.omit()
-      
+
       # Define study area with buffer
-      # bbox <- st_bbox(presence_pts)
-      # buffer_deg <- input$buffer_km / 111 # km to degrees (approximate)
-      # study_extent <<- extent(
-      #   bbox["xmin"] - buffer_deg,
-      #   bbox["xmax"] + buffer_deg,
-      #   bbox["ymin"] - buffer_deg,
-      #   bbox["ymax"] + buffer_deg
-      # )
       min_lon <- min(coords$lon) - 1
       max_lon <- max(coords$lon) + 1
       min_lat <- min(coords$lat) - 1
       max_lat <- max(coords$lat) + 1
-      
+
 
       study_extent <- ext(min_lon, max_lon, min_lat, max_lat)
       # Get environmental data
       incProgress(0.3, detail = "Downloading environmental data")
-      tryCatch({
-        bioclim_list <- list()
+      tryCatch(
+        {
+          bioclim_list <- list()
 
-        bio <- worldclim_global(var = "bio", res = 10, path = tempdir())
-        
-        bio <- bio[[c(1,5,6,12)]]
-        names(bio) <- stringr::str_remove_all(names(bio), "wc2.1_10m_")
+          bio <- worldclim_global(var = "bio", res = 10, path = tempdir())
 
-        # bio[[c("bio_1","bio_5","bio_12")]]
-        bio_filter <- bio[[c(input$bioclim_vars)]]
-        env_stack <- crop(bio_filter, study_extent)
-        # for(var in input$bioclim_vars) {
-        #   bioclim_list[[var]] <- var
-        # }
-        # bio <<- geodata::worldclim_global(var = "bio", res = 10, path=tempdir())
-        # tt <<- input$bioclim_vars
-        # bio
-        # bio <<- bio[[input$bioclim_vars]]
-        # bioclim_data(bio)
-        # bioclim <- raster::getData('worldclim', var = 'bio', res = 10)
-        # bioclim <- bioclim[[input$bioclim_vars]]
-        # env_stack <<- crop(bio, study_extent)
-        # env_stack <- bio_crop
-        # Generate pseudo-absence points
-        incProgress(0.2, detail = "Generating background points")
-        # bg_points <- randomPoints(bioclim[[1]], n = 1000) %>%
-        #   as.data.frame()
-        
-        bg_points <- spatSample(env_stack, size = 1000, 
-                                method = "random", na.rm = TRUE, as.points = TRUE)
-        bg_coords <- crds(bg_points)
-        colnames(bg_coords) <- c("lon", "lat")
-        bg_env <- terra::extract(env_stack, bg_points)
-        bg_env <- cbind(bg_coords, bg_env)
-        bg_env$presence <- 0
-        
-        # names(bg_points) <- c("lon", "lat")
-        
-        # Extract environmental data for presence points
-        incProgress(0.1, detail = "Extracting environmental data")
-        # coords <- st_coordinates(presence_pts)
-        # presence_env <- raster::extract(bioclim, coords) %>%
-        #   as.data.frame() %>%
-        #   mutate(presence = 1)
-        presence_env <- terra::extract(env_stack, coords)
-        presence_env <- cbind(coords, presence_env)
-        presence_env$presence <- 1
-        # Extract environmental data for background points
-        # bg_env <- raster::extract(bioclim, bg_points) %>%
-        #   as.data.frame() %>%
-        #   mutate(presence = 0)
-        
-        # Combine for model training
-        model_data <- rbind(presence_env, bg_env)
-        model_data <<- na.omit(model_data)
-        model_data
+          bio <- bio[[c(1, 5, 6, 12)]]
+          names(bio) <- stringr::str_remove_all(names(bio), "wc2.1_10m_")
 
+          bio_filter <- bio[[c(input$bioclim_vars)]]
+          env_stack <- crop(bio_filter, study_extent)
 
-        # Build random forest model
-        incProgress(0.2, detail = "Building random forest model")
-        # rf_model <<- randomForest(
-        #   x =  model_data %>% select(names(env_stack)),
-        #     # model_data[, !names(model_data) %in% "presence"],
-        #   y = as.factor(model_data$presence),
-        #   ntree = input$ntree,
-        #   importance = TRUE
-        # )
-        
-        rf_model <- ranger::ranger(presence ~ . , data=model_data %>% select(presence, names(env_stack)),
-                           num.trees = 500, na.action = "na.omit", importance = "impurity")
-        
-        sdm_model(rf_model)
-        
-        # Generate prediction
-        incProgress(0.2, detail = "Generating prediction map")
-        pred <- terra::predict(env_stack, rf_model, na.rm=T)
+          # Generate pseudo-absence points
+          incProgress(0.2, detail = "Generating background points")
 
-        prediction_raster(pred)
-        
+          bg_points <- spatSample(env_stack,
+            size = 1000,
+            method = "random", na.rm = TRUE, as.points = TRUE
+          )
+          bg_coords <- crds(bg_points)
+          colnames(bg_coords) <- c("lon", "lat")
+          bg_env <- terra::extract(env_stack, bg_points)
+          bg_env <- cbind(bg_coords, bg_env)
+          bg_env$presence <- 0
 
-        # Show model summary
-        output$model_summary <- renderPrint({
-          rf_model
-        })
-        
-      }, error = function(e) {
-        showNotification(paste("Error:", e$message), type = "error")
-      })
+          # Extract environmental data for presence points
+          incProgress(0.1, detail = "Extracting environmental data")
+
+          presence_env <- terra::extract(env_stack, coords)
+          presence_env <- cbind(coords, presence_env)
+          presence_env$presence <- 1
+
+          # Combine for model training
+          model_data <- rbind(presence_env, bg_env)
+          model_data <- na.omit(model_data)
+
+          # Build random forest model via ranger
+          incProgress(0.2, detail = "Building random forest model")
+
+          rf_model <- ranger::ranger(presence ~ .,
+            data = model_data %>% select(presence, names(env_stack)),
+            num.trees = 500, na.action = "na.omit", importance = "impurity"
+          )
+
+          sdm_model(rf_model)
+
+          # Generate prediction
+          incProgress(0.2, detail = "Generating prediction map")
+          pred <- terra::predict(env_stack, rf_model, na.rm = T)
+
+          prediction_raster(pred)
+
+          # Show model summary
+          output$model_summary <- renderPrint({
+            rf_model
+          })
+        },
+        error = function(e) {
+          showNotification(paste("Error:", e$message), type = "error")
+        }
+      )
     })
   })
-  
+
   # Display prediction map
   output$map_prediction <- renderLeaflet({
     req(prediction_raster())
-    
+
     pred <- prediction_raster()
     data <- presence_data()
-    
+
     # Color palette for predictions
     pal <- colorNumeric(
       palette = colorRampPalette(c("#2c7bb6", "#abd9e9", "#ffffbf", "#fdae61", "#d7191c"))(100),
       domain = c(0, 1)
     )
-    
+
     leaflet() %>%
       addProviderTiles(providers$Esri.WorldTopoMap) %>%
       addRasterImage(pred, colors = pal, opacity = 0.7) %>%
@@ -269,7 +229,7 @@ server <- function(input, output, session) {
   # Variable importance plot
   output$variable_importance <- renderPlot({
     req(sdm_model())
-    
+
     # imp <- importance(rf_model)
 
     imp <- ranger::importance(sdm_model())
@@ -277,24 +237,16 @@ server <- function(input, output, session) {
       Variable = names(imp),
       Importance = imp
     )
-    
+
     # Create nicer labels for bioclim variables
-    var_labels <<- c(
-      names(imp)
-      # "bio1" = "Annual Mean Temperature",
-      # "bio12" = "Annual Precipitation",
-      # "bio4" = "Temperature Seasonality",
-      # "bio15" = "Precipitation Seasonality"
-    )
-    
-    imp
-    
+    var_labels <- names(imp)
+
     imp_df$Label <- var_labels[imp_df$Variable]
     imp_df$Label[is.na(imp_df$Label)] <- imp_df$Variable[is.na(imp_df$Label)]
-    
+
     # Sort by importance
     imp_df <- imp_df[order(imp_df$Importance, decreasing = TRUE), ]
-    
+
     # Plot
     par(mar = c(8, 4, 2, 2))
     barplot(
--- app2.R
+++ app2.R
@@ -18,16 +18,18 @@ ui <- page_sidebar(
     selectInput("speciesColumn", "Species Column", choices = NULL),
     numericInput("resolution", "Resolution (arc-minutes)", value = 10, min = 2.5, max = 30),
     checkboxGroupInput("bioclimVars", "Bioclimatic Variables",
-                       choices = c("Annual Mean Temperature" = "tavg",
-                                   "Annual Precipitation" = "prec",
-                                   "Max Temperature of Warmest Month" = "tmax",
-                                   "Min Temperature of Coldest Month" = "tmin"),
-                       selected = c("tavg", "prec")),
+      choices = c(
+        "Annual Mean Temperature" = "tavg",
+        "Annual Precipitation" = "prec",
+        "Max Temperature of Warmest Month" = "tmax",
+        "Min Temperature of Coldest Month" = "tmin"
+      ),
+      selected = c("tavg", "prec")
+    ),
     checkboxInput("includeElevation", "Include Elevation", TRUE),
     numericInput("ntrees", "Number of Trees", value = 500, min = 100, max = 2000),
     actionButton("runModel", "Run Model", class = "btn-primary")
   ),
-  
   layout_columns(
     card(card_header("Data Preview"), tableOutput("dataPreview")),
     navset_card_tab(
@@ -42,7 +44,7 @@ server <- function(input, output, session) {
   species_data <- reactiveVal(NULL)
   rf_model <- reactiveVal(NULL)
   prediction_raster <- reactiveVal(NULL)
-  
+
   observeEvent(input$speciesFile, {
     req(input$speciesFile)
     data <- read_csv(input$speciesFile$datapath)
@@ -51,128 +53,137 @@ server <- function(input, output, session) {
     updateSelectInput(session, "lonColumn", choices = colnames(data))
     updateSelectInput(session, "speciesColumn", choices = colnames(data))
   })
-  
+
   output$dataPreview <- renderTable({
     req(species_data())
     head(species_data(), 10)
   })
-  
+
   observeEvent(input$runModel, {
     req(species_data(), input$latColumn, input$lonColumn, input$speciesColumn, input$bioclimVars)
-    
+
     data <- species_data()
     coords <- data %>%
       select(lon = !!input$lonColumn, lat = !!input$latColumn) %>%
       na.omit()
-    
+
     # Determine extent with buffer
     min_lon <- min(coords$lon) - 1
     max_lon <- max(coords$lon) + 1
     min_lat <- min(coords$lat) - 1
     max_lat <- max(coords$lat) + 1
-    
-    withProgress(message = 'Processing...', value = 0, {
+
+    withProgress(message = "Processing...", value = 0, {
       # Download bioclimatic variables
       incProgress(0.2, detail = "Downloading climate data")
       bioclim_list <- list()
-      for(var in input$bioclimVars) {
+      for (var in input$bioclimVars) {
         bio <- worldclim_global(var = var, res = input$resolution, path = tempdir())
         bio_crop <- crop(bio, ext(min_lon, max_lon, min_lat, max_lat))
         bioclim_list[[var]] <- bio_crop
       }
 
       # Get elevation data if requested
-      if(input$includeElevation) {
+      if (input$includeElevation) {
         incProgress(0.4, detail = "Downloading elevation data")
         elev <- get_elev_raster(locations = coords, z = input$resolution, src = "aws")
         elev <- rast(elev)
         names(elev) <- "elevation"
         bioclim_list[["elevation"]] <- elev
       }
-      
+
       # Create environmental stack and extract at occurrence points
       incProgress(0.6, detail = "Preparing model data")
       env_stack <- rast(bioclim_list)
-      
+
       occurrence_data <- terra::extract(env_stack, coords)
       occurrence_data <- cbind(coords, occurrence_data)
       occurrence_data$presence <- 1
-      
+
       # Generate background points
-      bg_points <- spatSample(env_stack, size = nrow(occurrence_data) * 3, 
-                              method = "random", na.rm = TRUE, as.points = TRUE)
+      bg_points <- spatSample(env_stack,
+        size = nrow(occurrence_data) * 3,
+        method = "random", na.rm = TRUE, as.points = TRUE
+      )
       bg_coords <- crds(bg_points)
       colnames(bg_coords) <- c("lon", "lat")
       bg_data <- terra::extract(env_stack, bg_points)
       bg_data <- cbind(bg_coords, bg_data)
       bg_data$presence <- 0
-      
+
       # Combine data and train model
       model_data <- rbind(occurrence_data, bg_data)
       model_data <- na.omit(model_data)
       model_data$presence <- factor(model_data$presence)
-      
+
       print(names(env_stack))
-      
+
       incProgress(0.8, detail = "Training random forest model")
       model_formula <- as.formula(paste("presence ~", paste(names(env_stack), collapse = " + ")))
-      
-      rf <- randomForest(model_formula, data = model_data, 
-                         ntree = input$ntrees, importance = TRUE)
+
+      rf <- randomForest(model_formula,
+        data = model_data,
+        ntree = input$ntrees, importance = TRUE
+      )
       rf_model(rf)
-      
+
       # Predict to the entire region
       prediction <- predict(env_stack, rf, type = "prob")
       prediction_raster(prediction)
-      
+
       incProgress(1.0, detail = "Done!")
     })
   })
-  
+
   output$map <- renderLeaflet({
     req(prediction_raster())
-    
+
     pred_rast <- prediction_raster()
-    
+
     # Get species data for plotting occurrence points
     data <- species_data()
     occurrence_points <- data %>%
       select(lon = !!input$lonColumn, lat = !!input$latColumn) %>%
       na.omit()
-    
+
     # Create leaflet map
     leaflet() %>%
       addProviderTiles(providers$CartoDB.Positron) %>%
-      addRasterImage(pred_rast, colors = colorRampPalette(c("blue", "green", "yellow", "red"))(100),
-                     opacity = 0.7, group = "Prediction") %>%
-      addCircleMarkers(data = occurrence_points, ~lon, ~lat, 
-                       radius = 4, color = "black", fillColor = "white",
-                       fillOpacity = 0.7, weight = 1.5, group = "Occurrences") %>%
+      addRasterImage(pred_rast,
+        colors = colorRampPalette(c("blue", "green", "yellow", "red"))(100),
+        opacity = 0.7, group = "Prediction"
+      ) %>%
+      addCircleMarkers(
+        data = occurrence_points, ~lon, ~lat,
+        radius = 4, color = "black", fillColor = "white",
+        fillOpacity = 0.7, weight = 1.5, group = "Occurrences"
+      ) %>%
       addLayersControl(
         overlayGroups = c("Prediction", "Occurrences"),
         options = layersControlOptions(collapsed = FALSE)
       )
   })
-  
+
   output$varImpPlot <- renderPlot({
     req(rf_model())
-    
+
     var_imp <- importance(rf_model())
     var_imp_df <- data.frame(
       Variable = rownames(var_imp),
       MeanDecreaseAccuracy = var_imp[, "%IncMSE"]
     )
-    
+
     var_imp_df <- var_imp_df[order(var_imp_df$MeanDecreaseAccuracy, decreasing = TRUE), ]
-    
+
     # Make variable names more readable
     var_imp_df$Variable <- recode(var_imp_df$Variable,
-                                  "tavg" = "Annual Mean Temperature",
-                                  "tmax" = "Max Temp Warmest Month",
-                                  "tmin" = "Min Temp Coldest Month",
-                                  "prec" = "Annual Precipitation")
-                                  # "elevation" = "Elevation")
-    
+      "tavg" = "Annual Mean Temperature",
+      "tmax" = "Max Temp Warmest Month",
+      "tmin" = "Min Temp Coldest Month",
+      "prec" = "Annual Precipitation"
+    )
+    # "elevation" = "Elevation")
+
     ggplot(var_imp_df, aes(x = reorder(Variable, MeanDecreaseAccuracy), y = MeanDecreaseAccuracy)) +
       geom_bar(stat = "identity", fill = "steelblue") +
       coord_flip() +
